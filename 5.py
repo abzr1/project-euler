f3e1c98c699a9006a2a3b4d136e0bfb8d9d21580
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2022-10-23 22:35:47 +0100

    Remove '.' from comment

--- 5.py
+++ 5.py
@@ -1,7 +1,7 @@
 x = 20
 
 while True:
-    # Some factors can be removed because e.g. if x % 20 == 0 then x % 2 == 0 and x % 10 == 0.
+    # Some factors can be removed because e.g. if x % 20 == 0 then x % 2 == 0 and x % 10 == 0
     if (
         x % 11 == 0
         and x % 13 == 0
