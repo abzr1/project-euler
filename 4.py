f81371a4cb54613bb1baadd4818c67cf579c55f9
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2022-10-23 22:21:53 +0100

    Optimise 4

--- 4.py
+++ 4.py
@@ -1,19 +1,16 @@
 def is_palindrome(n):
-    if n == int(str(n)[::-1]):
-        return True
-    else:
-        return False
+    return n == int(str(n)[::-1])
 
 
 a = 0
 b = 0
-largest_palindrome = 0
+largest = 0
 
 for i in range(100, 1000):
     for x in range(100, 1000):
-        if is_palindrome(i * x) and i * x > largest_palindrome:
-            largest_palindrome = i * x
+        if is_palindrome(i * x) and i * x > largest:
+            largest = i * x
             a = i
             b = x
 
-print(f"{a} * {b} = {largest_palindrome} is the largest palindrome")
+print(f"{a} * {b} = {largest} is the largest palindrome")
