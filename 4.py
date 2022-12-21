43c1ac1e01b6599f710cc96279efdbabd7f7f63c
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2022-12-21 19:36:05 +0000

    Update 4.py
    Remove redundant variables

--- 4.py
+++ 4.py
@@ -2,15 +2,11 @@ def is_palindrome(n):
     return n == int(str(n)[::-1])
 
 
-a = 0
-b = 0
 largest = 0
 
 for i in range(100, 1000):
     for x in range(100, 1000):
         if is_palindrome(i * x) and i * x > largest:
             largest = i * x
-            a = i
-            b = x
 
 print(largest)
